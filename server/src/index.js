5ee4abc56a84a8a9ed1800886415824e1d073651
Anjelo Petkov <rapid3fighter@hotmail.com>
2019-05-31 17:39:20 +0300

    Added scafollded Angular project

--- server/src/index.js
+++ server/src/index.js
@@ -14,8 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
 	req.context = {
-		models,
-		me: models.users[1]
+		models
 	};
 	next();
 });
@@ -24,7 +23,7 @@ app.use('/users', routes.user);
 app.use('/books', routes.book);
 
 app.get('/authenticated', (req, res) => {
-	res.send(req.context.users[req.context.me.id]);
+	res.send(req.context.users[0]);
 });
 
 const eraseDatabaseOnSync = true;
@@ -36,7 +35,6 @@ connectDb().then(async () => {
 			models.Book.deleteMany({})
 		]);
 	}
-
 	app.listen(process.env.PORT, () =>
 		console.log(`Example app listening on port ${process.env.PORT}!`)
 	);
