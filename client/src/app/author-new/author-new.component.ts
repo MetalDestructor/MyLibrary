8b0d6c0d69a0db3e8e5041d8b3c2c8ac33ae8f40
MetalDestructor <rapid3fighter@hotmail.com>
2019-06-13 15:39:37 +0300

    New author button in authors page

--- client/src/app/author-new/author-new.component.ts
+++ client/src/app/author-new/author-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-author-new',
@@ -10,13 +11,15 @@ export class AuthorNewComponent implements OnInit {
 
   authorData = {}
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
   submit(){
     this.apiService.postAuthor(this.authorData);
+    this.router.navigate(['/authors']);
   }
 
 }
